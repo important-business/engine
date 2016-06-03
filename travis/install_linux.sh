44b152faf29f87f268547c5bf2aadf22605c6c44
Keegan Morrow <Korrow@gmail.com>
2016-06-02 22:24:41 -0700

    Travis: Download CMake over HTTPS
    This should fix the error encountered with build #1

--- travis/install_linux.sh
+++ travis/install_linux.sh
@@ -6,5 +6,5 @@ echo "########################################################################"
 
 apt-get install -qq libsdl2-dev libsdl2-image-dev liblog4cplus-dev gcc-4.8 g++-4.8
 update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
-wget http://www.cmake.org/files/v3.3/cmake-3.3.1-Linux-x86_64.tar.gz -O cmake.tar.gz
+wget https://cmake.org/files/v3.3/cmake-3.3.1-Linux-x86_64.tar.gz -O cmake.tar.gz
 tar -xzf cmake.tar.gz -C $HOME
