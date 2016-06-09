af9309e3ee51e28450125a7c6eddb22ff0c5b8a2
Keegan MOrrow <Korrow@gmail.com>
2016-06-08 22:11:22 -0700

    Play around with glfw3 a little bit
    Some pretty awesome looking tutorials for OpenGL use glfw, so creating
    a branch to start playing around with it. Even if we end up using SDL2
    to create an OpenGL context in the long term the concepts should apply!

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
-#include "core/example_module.hpp"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 int main(int argc, char* argv[])
 {
     std::cout << "Yay, you built code!\n";
-    auto a = core::ExampleModule(4);
-    auto i = a.getme();
-    std::cout << "Returned value " << i << "\n";
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    return 0;
 }
