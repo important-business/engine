fd6a6f1e3370fbedfe6fe7fc7efa826819cd4669
Keegan Morrow <Korrow@gmail.com>
2016-06-28 15:40:16 -0700

    CMake: Have FetchGtest define a function
    Have FetchGtest define a function that must be called. This will
    potentially allow arguments to configure how Google Test is built.

--- cmake/modules/FetchGtest.cmake
+++ cmake/modules/FetchGtest.cmake
@@ -1,27 +1,28 @@
-# Required for google test
-find_package(Threads REQUIRED)
+function(FetchGoogleTest)
+    # Required for google test
+    find_package(Threads REQUIRED)
 
-# Enable ExternalProject CMake module
-include(ExternalProject)
+    # Enable ExternalProject CMake module
+    include(ExternalProject)
 
-# Download and install GoogleTest
-# Based off http://www.kaizou.org/2014/11/gtest-cmake/
-# Thanks to David Corvoysier and Henry Schreiner
-# TODO: Update Gtest?
-ExternalProject_Add(
-    gtest
-    GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG release-1.7.0
-    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/gtest
-    # Disable install step
-    INSTALL_COMMAND ""
-)
+    # Download and install GoogleTest
+    # Based off http://www.kaizou.org/2014/11/gtest-cmake/
+    # Thanks to David Corvoysier and Henry Schreiner
+    # TODO: Update Gtest?
+    ExternalProject_Add(
+        gtest
+        GIT_REPOSITORY https://github.com/google/googletest.git
+        GIT_TAG release-1.7.0
+        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/gtest
+        # Disable install step
+        INSTALL_COMMAND ""
+    )
 
-ExternalProject_Get_Property(gtest source_dir binary_dir)
+    ExternalProject_Get_Property(gtest source_dir binary_dir)
 
-# Create an interface library for tests to link against
-add_library(libgtest INTERFACE)
-add_dependencies(libgtest gtest)
-target_link_libraries(libgtest INTERFACE Threads::Threads "${binary_dir}/libgtest.a")
-target_include_directories(libgtest INTERFACE "${source_dir}/include")
-
+    # Create an interface library for tests to link against
+    add_library(libgtest INTERFACE)
+    add_dependencies(libgtest gtest)
+    target_link_libraries(libgtest INTERFACE Threads::Threads "${binary_dir}/libgtest.a")
+    target_include_directories(libgtest INTERFACE "${source_dir}/include")
+endfunction(FetchGoogleTest)
