bbb71bc204361e8b189819b2dc946a80d4fcb9f7
Keegan Morrow <Korrow@gmail.com>
2016-06-28 15:05:50 -0700

    CMake: Move getting GTest into its own module
    May want to rethink the interface between the FetchGtest module and
    the main CMakefiles, but this at least modularizes things a bit.

---
+++ cmake/modules/FetchGtest.cmake
@@ -0,0 +1,26 @@
+# Required for google test
+find_package(Threads REQUIRED)
+
+# Enable ExternalProject CMake module
+include(ExternalProject)
+
+# Download and install GoogleTest
+# Based off http://www.kaizou.org/2014/11/gtest-cmake/
+# Thanks to David Corvoysier and Henry Schreiner
+# TODO: Update Gtest?
+ExternalProject_Add(
+    gtest
+    URL https://googletest.googlecode.com/files/gtest-1.7.0.zip
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/gtest
+    # Disable install step
+    INSTALL_COMMAND ""
+)
+
+ExternalProject_Get_Property(gtest source_dir binary_dir)
+
+# Create an interface library for tests to link against
+add_library(libgtest INTERFACE)
+add_dependencies(libgtest gtest)
+target_link_libraries(libgtest INTERFACE Threads::Threads "${binary_dir}/libgtest.a")
+target_include_directories(libgtest INTERFACE "${source_dir}/include")
+
