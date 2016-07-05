4fe9467bd508a5de79bd79bc30425987e6d29246
Keegan Morrow <Korrow@gmail.com>
2016-07-05 15:27:00 -0700

    Make style an option in ClangFormat CMake module
    Allow the style to be specified in the ClangFormat module. This allows
    a .clang-format file to be used, or one of the defaults to be specified.
    
    Argument defaults to 'file', which is what our project uses, and what allows
    the style options to be configured.

--- cmake/modules/ClangFormat.cmake
+++ cmake/modules/ClangFormat.cmake
@@ -1,12 +1,18 @@
 function(ClangFormat)
-    set(options OPTIONAL FAST)
-    set(oneValueArgs DESTINATION RENAME)
+    # Parse Arguments
+    set(options OPTIONAL)
+    set(oneValueArgs DESTINATION STYLE)
     set(multiValueArgs TARGETS CLANGFORMAT_FILES)
     cmake_parse_arguments(ClangFormat "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
 
+    # Set defaults for unprovided arguments
+    if(NOT ClangFormat_STYLE)
+        set(ClangFormat_STYLE "file")
+    endif()
+
     add_custom_target(
             clangformat
-            COMMAND /usr/bin/clang-format -i ${ClangFormat_CLANGFORMAT_FILES}
+            COMMAND /usr/bin/clang-format -i ${ClangFormat_CLANGFORMAT_FILES} -style=${ClangFormat_STYLE}
     )
 
 endfunction(ClangFormat)
