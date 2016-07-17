0d0f44b47ef561d000fec2e80f3e9ee29df50e41
Keegan Morrow <Korrow@gmail.com>
2016-07-17 10:51:03 -0700

    Travis: Download CMake to home instead of repo
    This is to prevent the repository from being incorrectly marked as
    dirty by the check_clang_format script.

--- travis/linux_cmake.sh
+++ travis/linux_cmake.sh
@@ -8,7 +8,7 @@ echo "########################################################################"
 echo "# Travis CMake upgrade script"
 echo "########################################################################"
 
-wget https://cmake.org/files/v3.3/cmake-3.3.1-Linux-x86_64.tar.gz -O cmake.tar.gz --no-check-certificate
-tar -xzf cmake.tar.gz -C $HOME
+wget https://cmake.org/files/v3.3/cmake-3.3.1-Linux-x86_64.tar.gz -O $HOME/cmake.tar.gz --no-check-certificate
+tar -xzf $HOME/cmake.tar.gz -C $HOME
 
 export PATH=$HOME/cmake-3.3.1-Linux-x86_64/bin:$PATH
