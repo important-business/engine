286c00784cf355742336f8b1a17b67476bd0e91d
Keegan Morrow <Korrow@gmail.com>
2016-07-17 09:47:13 -0700

    CMake: Allow override of ClangFormat binary
     * Update the ClangFormat CMake module to allow the binary to be
     overridden by the environment variable CLANG_FORMAT
     * Update Travis configuration to install clang-format 3.7 and specify
       it as being the binary to use

--- cmake/modules/ClangFormat.cmake
+++ cmake/modules/ClangFormat.cmake
@@ -10,6 +10,14 @@ function(ClangFormat)
         set(ClangFormat_STYLE "file")
     endif()
 
+    if (DEFINED ENV{CLANG_FORMAT})
+        set(CLANG_FORMAT_BIN "$ENV{CLANG_FORMAT}")
+        message("Using ${CLANG_FORMAT_BIN} for clangformat")
+    else()
+        set(CLANG_FORMAT_BIN "clang-format")
+    endif()
+
+
     add_custom_target(
             clangformat
             COMMAND clang-format -i ${ClangFormat_CLANGFORMAT_FILES} -style=${ClangFormat_STYLE}
