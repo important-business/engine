45343dc098a51f353df10729c4525fa629efae6b
Keegan Morrow <Korrow@gmail.com>
2016-11-14 18:44:30 -0800

    Add tool to check JSON validity/format
    Add a jsoncheck tool that is used to check that the validity
    and formatting of the JSON files match what the game itself would
    output. Updated existing files to conform to its output style.
    
    The jsoncpp serialization does cause objects to have a trailing
    space following the object name. We could update the write method
    to strip newlines in the future if desired.

--- tools/json_check.sh
+++ tools/json_check.sh
@@ -2,10 +2,9 @@
 failures=0
 for f in $(find . -name '*.json' -not -path './build/*' ); do
     echo "Checking JSON file $f"
-    fixed=$(cat $f | python -m json.tool)
+    ./build/src/tools/jsoncheck -f $f -u
     if [ $? -eq 0 ];then
         echo "Check passed, writing $f in updated format"
-        echo "$fixed" > $f
     else
         echo "Error encountered in JSON file $f"
         failures=$((failures+1))
