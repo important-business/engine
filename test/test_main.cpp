ef186f251c6d6a948d80b8acb87cd0a31e6bb345
Keegan Morrow <Korrow@gmail.com>
2016-12-01 17:33:48 -0800

    Test Main: Set up logging
    This will prevent segfaults that would otherwise happen in tests.

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,8 +1,10 @@
+#include "core/logging.hpp"
 #include <iostream>
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
 int main(int argc, char* argv[])
 {
+    core::logging_init();
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
