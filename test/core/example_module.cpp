1417782aa811b2cb3cf5733778017515af6928df
Keegan Morrow <Korrow@gmail.com>
2016-12-01 17:33:34 -0800

    Update CMake to support test infrastructure
    Update how CMake is structured to better work with the test
    infrastructure.
     * Moved library download to top level CMake file for both to access
     * Don't glob for test cases anymore
     * Link tests to third party libraries

--- test/core/example_module.cpp
+++ test/core/example_module.cpp
@@ -6,24 +6,24 @@ using namespace core;
 TEST(module_get_set_test, zero_test)
 {
     auto a = core::ExampleModule(0);
-    ASSERT_EQ(0, a.getme());
-    a.setme(0);
-    ASSERT_EQ(0, a.getme());
+    ASSERT_EQ(0, a.get_me());
+    a.set_me(0);
+    ASSERT_EQ(0, a.get_me());
 }
 
 TEST(module_get_set_test, positive_test)
 {
     auto a = core::ExampleModule(4);
-    ASSERT_EQ(4, a.getme());
-    a.setme(15);
-    ASSERT_EQ(15, a.getme());
+    ASSERT_EQ(4, a.get_me());
+    a.set_me(15);
+    ASSERT_EQ(15, a.get_me());
 }
 
 /* This test fails on purpose! */
 TEST(module_get_set_test, negative_test)
 {
     auto a = core::ExampleModule(-2);
-    ASSERT_EQ(-2, a.getme());
-    a.setme(-5);
-    ASSERT_EQ(-5, a.getme());
+    ASSERT_EQ(-2, a.get_me());
+    a.set_me(-5);
+    ASSERT_EQ(-5, a.get_me());
 }
