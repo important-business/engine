ed716aa133ef9c52d049d4b46daaa2205c8099a9
Keegan Morrow <Korrow@gmail.com>
2016-12-02 16:06:24 -0800

    Build anax and sdl_wrap as static libraries

--- cmake/modules/FetchAnax.cmake
+++ cmake/modules/FetchAnax.cmake
@@ -1,12 +1,15 @@
 # TODO: Add option to link statically
 function(FetchAnax)
     set(options)
-    set(oneValueArgs GITVERSION)
+    set(oneValueArgs GITVERSION BUILD_SHARED)
     set(multiValueArgs)
     cmake_parse_arguments(FetchAnax "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
     # Set defaults for unprovided arguments
     if(NOT FetchAnax_GITVERSION)
         set(FetchAnax_GITVERSION "master")
+    endif()
+    if(FetchAnax_BUILD_SHARED STREQUAL "")
+        set(FetchAnax_BUILD_SHARED "false")
     endif()
 
     # Enable ExternalProject CMake module
@@ -23,17 +26,26 @@ function(FetchAnax)
         PREFIX ${CMAKE_CURRENT_BINARY_DIR}/anax
         # Disable install step
         INSTALL_COMMAND ""
+        CMAKE_CACHE_ARGS "-DBUILD_SHARED_LIBS:BOOL=${FetchAnax_BUILD_SHARED}"
     )
 
+    if(FetchAnax_BUILD_SHARED)
+        set(LIBRARY_SUFFIX ${CMAKE_SHARED_LIBRARY_SUFFIX})
+        set(LIBRARY_PREFIX "/lib/${CMAKE_SHARED_LIBRARY_PREFIX}")
+    else()
+        set(LIBRARY_SUFFIX ${CMAKE_STATIC_LIBRARY_SUFFIX})
+        set(LIBRARY_PREFIX ${CMAKE_STATIC_LIBRARY_PREFIX})
+    endif()
     ExternalProject_Get_Property(anax_proj source_dir binary_dir)
 
     # Create an interface library for tests to link against
     add_library(anax INTERFACE)
     add_dependencies(anax anax_proj)
+
     target_link_libraries(
         anax
         INTERFACE
-        "${binary_dir}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}anax${CMAKE_SHARED_LIBRARY_SUFFIX}"
+        "${binary_dir}/${LIBRARY_PREFIX}anax${LIBRARY_SUFFIX}"
     )
     # TODO: Is this the best way to export the include directory?
     set(ANAX_INCLUDE_DIR "${source_dir}/include" "${binary_dir}/include" PARENT_SCOPE)
