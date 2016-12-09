5f6563c8ad02c995ee971de9694b19d42b8f4e77
Keegan Morrow <Korrow@gmail.com>
2016-12-08 17:13:54 -0800

    Add OClint support to CMake

---
+++ cmake/modules/Oclint.cmake
@@ -0,0 +1,37 @@
+if(NOT TARGET oclint)
+    add_custom_target (
+        oclint
+        )
+endif()
+
+if (DEFINED ENV{OCLINT})
+    set(OCLINT_BIN "$ENV{OCLINT}")
+    message("Using ${OCLINT_BIN} for oclint")
+else()
+    set(OCLINT_BIN "oclint")
+endif()
+
+function(OcLint)
+    # Parse Arguments
+    set(options OPTIONAL)
+    set(oneValueArgs DESTINATION TARGETNAME ENABLE CXX_STANDARD)
+    set(multiValueArgs TARGETS FILES INCLUDEDIRS)
+    cmake_parse_arguments(OcLint "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
+
+    # Set defaults for unprovided arguments
+    if(NOT OcLint_ENABLE)
+        set(OcLint_ENABLE "warning,performance")
+    endif()
+
+    foreach(dir ${OcLint_INCLUDEDIRS})
+        set(INCLUDE_ARGS "${INCLUDE_ARGS}" "-I${dir}")
+    endforeach()
+
+    add_custom_target(
+        oclint_${OcLint_TARGETNAME}
+        COMMAND ${OCLINT_BIN} ${OcLint_FILES} -- ${INCLUDE_ARGS} -std=c++${OcLint_CXX_STANDARD}
+    )
+
+add_dependencies(oclint oclint_${OcLint_TARGETNAME})
+
+endfunction(OcLint)
