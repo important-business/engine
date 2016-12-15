4e0f8624d897f694393b5e501c4b1122b9fb4cf5
Keegan Morrow <Korrow@gmail.com>
2016-12-14 16:21:29 -0800

    Travis: Fix issue with website deploy key

--- ci/travis/deploy_website.sh
+++ ci/travis/deploy_website.sh
@@ -34,10 +34,12 @@ ENCRYPTED_IV_VAR="encrypted_${DEPLOY_KEY_ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
+DEPLOY_KEY_ENC="${ENGINEDIR}/ci/travis/keys/deploy_key.enc"
+DEPLOY_KEY="${HOME}/deploy_key"
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ${DEPLOY_KEY_ENC} -out ${DEPLOY_KEY} -d
+chmod 600 ${DEPLOY_KEY}
 eval `ssh-agent -s`
-ssh-add deploy_key
+ssh-add ${DEPLOY_KEY}
 
 # # Now that we're all set up, we can push.
 git push $SSH_REPO HEAD
