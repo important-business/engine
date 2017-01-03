03ca47e3fc4b2d02e4b03712991a37f4a8b5e406
Keegan Morrow <Korrow@gmail.com>
2017-01-03 14:29:16 -0800

    Fix math tests
    Make them use the ASSERT_FLOAT_EQ macro and pass floats to the
    constructors.

--- test/core/math.cpp
+++ test/core/math.cpp
@@ -5,105 +5,105 @@ using namespace core;
 
 TEST(math_vector_add, add_zero_test)
 {
-    auto a = core::Vector(5, 5);
-    auto b = core::Vector(0, 0);
+    auto a = core::Vector(5.0f, 5.0f);
+    auto b = core::Vector(0.0f, 0.0f);
     auto result = a + b;
-    ASSERT_EQ(5, result.x);
-    ASSERT_EQ(5, result.y);
+    ASSERT_FLOAT_EQ(5.0f, result.x);
+    ASSERT_FLOAT_EQ(5.0f, result.y);
 }
 
 TEST(math_vector_add, add_positive_test)
 {
-    auto a = core::Vector(40, 50);
-    auto b = core::Vector(20, 42);
+    auto a = core::Vector(40.0f, 50.0f);
+    auto b = core::Vector(20.0f, 42.0f);
     auto result = a + b;
-    ASSERT_EQ(60, result.x);
-    ASSERT_EQ(92, result.y);
+    ASSERT_FLOAT_EQ(60.0f, result.x);
+    ASSERT_FLOAT_EQ(92.0f, result.y);
 }
 
 TEST(math_vector_add, add_negative_test)
 {
-    auto a = core::Vector(-400, -24);
-    auto b = core::Vector(-90, -201);
+    auto a = core::Vector(-400.0f, -24.0f);
+    auto b = core::Vector(-90.0f, -201.0f);
     auto result = a + b;
-    ASSERT_EQ(-490, result.x);
-    ASSERT_EQ(-225, result.y);
+    ASSERT_FLOAT_EQ(-490.0f, result.x);
+    ASSERT_FLOAT_EQ(-225.0f, result.y);
 }
 
 TEST(math_vector_mult, mult_zero_test)
 {
-    auto a = core::Vector(5, 5);
-    auto b = core::Vector(0, 0);
+    auto a = core::Vector(5.0f, 5.0f);
+    auto b = core::Vector(0.0f, 0.0f);
     auto result = a * b;
-    ASSERT_EQ(0, result.x);
-    ASSERT_EQ(0, result.y);
+    ASSERT_FLOAT_EQ(0.0f, result.x);
+    ASSERT_FLOAT_EQ(0.0f, result.y);
 }
 
 TEST(math_vector_mult, mult_positive_test)
 {
-    auto a = core::Vector(40, 50);
-    auto b = core::Vector(20, 42);
+    auto a = core::Vector(40.0f, 50.0f);
+    auto b = core::Vector(20.0f, 42.0f);
     auto result = a * b;
-    ASSERT_EQ(800, result.x);
-    ASSERT_EQ(2100, result.y);
+    ASSERT_FLOAT_EQ(800.0f, result.x);
+    ASSERT_FLOAT_EQ(2100.0f, result.y);
 }
 
 TEST(math_vector_mult, mult_negative_test)
 {
-    auto a = core::Vector(-400, -24);
-    auto b = core::Vector(-90, -201);
+    auto a = core::Vector(-400.0f, -24.0f);
+    auto b = core::Vector(-90.0f, -201.0f);
     auto result = a * b;
-    ASSERT_EQ(36000, result.x);
-    ASSERT_EQ(4824, result.y);
+    ASSERT_FLOAT_EQ(36000.0f, result.x);
+    ASSERT_FLOAT_EQ(4824.0f, result.y);
 }
 
 TEST(math_vector_sub, sub_zero_test)
 {
-    auto a = core::Vector(5, 5);
-    auto b = core::Vector(0, 0);
+    auto a = core::Vector(5.0f, 5.0f);
+    auto b = core::Vector(0.0f, 0.0f);
     auto result = a - b;
-    ASSERT_EQ(5, result.x);
-    ASSERT_EQ(5, result.y);
+    ASSERT_FLOAT_EQ(5.0f, result.x);
+    ASSERT_FLOAT_EQ(5.0f, result.y);
 }
 
 TEST(math_vector_sub, sub_positive_test)
 {
-    auto a = core::Vector(40, 50);
-    auto b = core::Vector(20, 42);
+    auto a = core::Vector(40.0f, 50.0f);
+    auto b = core::Vector(20.0f, 42.0f);
     auto result = a - b;
-    ASSERT_EQ(20, result.x);
-    ASSERT_EQ(8, result.y);
+    ASSERT_FLOAT_EQ(20.0f, result.x);
+    ASSERT_FLOAT_EQ(8.0f, result.y);
 }
 
 TEST(math_vector_sub, sub_negative_test)
 {
-    auto a = core::Vector(-400, -24);
-    auto b = core::Vector(-90, -201);
+    auto a = core::Vector(-400.0f, -24.0f);
+    auto b = core::Vector(-90.0f, -201.0f);
     auto result = a - b;
-    ASSERT_EQ(-310, result.x);
-    ASSERT_EQ(177, result.y);
+    ASSERT_FLOAT_EQ(-310.0f, result.x);
+    ASSERT_FLOAT_EQ(177.0f, result.y);
 }
 
 TEST(math_vector_dot, dot_zero_test)
 {
-    auto a = core::Vector(5, 5);
-    auto b = core::Vector(0, 0);
+    auto a = core::Vector(5.0f, 5.0f);
+    auto b = core::Vector(0.0f, 0.0f);
     auto result = a.dot(b);
-    ASSERT_EQ(0, result);
+    ASSERT_FLOAT_EQ(0.0f, result);
 }
 
 TEST(math_vector_dot, dot_positive_test)
 {
-    auto a = core::Vector(40, 50);
-    auto b = core::Vector(20, 42);
+    auto a = core::Vector(40.0f, 50.0f);
+    auto b = core::Vector(20.0f, 42.0f);
     auto result = a.dot(b);
-    ASSERT_EQ(2900, result);
+    ASSERT_FLOAT_EQ(2900.0f, result);
 }
 
 TEST(math_vector_dot, dot_negative_test)
 {
-    auto a = core::Vector(-400, -24);
-    auto b = core::Vector(-90, -201);
+    auto a = core::Vector(-400.0f, -24.0f);
+    auto b = core::Vector(-90.0f, -201.0f);
     auto result = a.dot(b);
-    ASSERT_EQ(40824, result);
+    ASSERT_FLOAT_EQ(40824.0f, result);
 }
